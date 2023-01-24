e202418e3ef318e7f2710e0d1e16a8a426717183
homeys <homeys@ysjangui-MacBookPro.local>
2023-01-25 00:47:22 +0900

    add searchText

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,139 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To Do List</h2>
+    <input class="form-control" v-model="searchText" placeholder="Search">
+    <hr/>
+    <TodoSimpleForm @add-todo="addTodo"/>
+    <!-- @emit을 통해 자식컴포넌트에서 받은 객체명="실행할 함수" -->
+    <div>{{ error }}</div>
+    <div v-if="!todo.length">Todo를 추가해주세요</div>
+    <TodoList :todo="todo" @toggle-todo="toggleTodo" @delete-thing="deletething"/>
+    <!-- :자식컴포넌트로 보낼 이름="데이터이름" : props -->
+    <hr/>
+    <!-- 페이지네이션 -->
+    <nav aria-label="Page navigaion example">
+      <ul class="pagination">
+        <li v-if="currentPage != 1" class="page-item">
+          <a class="page-link" @click="getTodo(currentPage -1)" style="cursor:pointer">Previous</a></li>
+        <li v-for="page in numberOfPages" :key="page" class="page-item" 
+        :class="currentPage == page ? 'active' : ''">
+        <a class="page-link" @click="getTodo(page)" style="cursor:pointer">{{ page }}</a></li>
+        <li class="page-item" v-if="numberOfPages != currentPage">
+          <a class="page-link" @click="getTodo(currentPage +1)" style="cursor:pointer">Next</a></li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, watch } from 'vue'; // watchEffect, 
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  components:{
+    TodoSimpleForm,TodoList
+  },
+  setup(){
+    const todo = ref([]);
+    const searchText = ref('');
+    const error = ref('');
+    const numberOfthings = ref(0);
+    let limit = 5;
+    const currentPage = ref(1);
+
+    
+    //총 페이지 수 계산
+    const numberOfPages = computed(() => {
+        return Math.ceil(numberOfthings.value/limit);
+    });
+
+    //검색기능을 watch로 구현
+    watch(searchText, () => {
+      getTodo(1);//검색데이터의 첫페이지
+    });
+
+    //검색기능 : 필터링 된 todo배열을 리턴함.
+    // const filteredTodos = computed(()=>{
+    //   if(searchText.value){
+    //     return todo.value.filter(thing =>{
+    //         //todo객체안의 subject(String)이 searchText를 포함하고 있으면 filteredTodos로 리턴한다.
+    //         return thing.subject.includes(searchText.value);
+    //     });
+    //   }
+    //     return todo.value;
+    // });
+    //todo리스트에서 값을 불러오기 (get)
+    const getTodo = async (page = currentPage.value) =>{
+      error.value = ''; 
+      currentPage.value=page;
+      try{
+        const res = await axios.get(
+          //백틱으로 감싸면 안에 자바스크립트 변수 입력가능
+          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+          );
+          numberOfthings.value = res.headers['x-total-count'];
+          todo.value = res.data;
+      }catch (err) {
+        console.log(err);
+        error.value = 'Something went wrong';
+      }
+    };
+    getTodo();
+
+    // todo 리스트에 값을 저장하기 (post)
+    const addTodo = async (thing) =>{
+      error.value = '';
+      try {      
+       await axios.post('http://localhost:3000/todos',{
+        subject: thing.subject,
+        completed: thing.completed,
+      });
+        getTodo(1);
+    } catch (err){
+        console.log(err);
+        error.value = 'Something went wrong';
+      }
+    };
+    //todo리스트 값 삭제하기
+    const deletething = async (index) => {
+      error.value = '';
+      try{
+      const id = todo.value[index].id;
+      await axios.delete('http://localhost:3000/todos/'+id);
+      getTodo(1);
+    } catch (err){
+      console.log(err);
+      error.value = 'Something went wrong';
+     }
+    };
+    //todo리스트 값 수정하기
+    const toggleTodo = async (index) =>{
+      const id = todo.value[index].id;
+      try{
+      await axios.patch('http://localhost:3000/todos/'+id,{
+        completed : !todo.value[index].completed
+      });
+      todo.value[index].completed = !todo.value[index].completed;
+     } catch (err) {
+        console.log(err);
+     }
+    };
+
+    return {
+      todo,searchText,//filteredTodos
+      error,numberOfPages,currentPage,
+      addTodo,deletething,toggleTodo,getTodo,
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.thing{
+  color: gray;
+  text-decoration: line-through;
 }
 </style>
+
---
+++ src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="card mt-2" v-for="(thing,index) in todo" :key="thing.id">
+          <div class="card-body p-2 d-flex align-itmes-center">
+            <div class="form-check flex-grow-1" >
+              <input class="form-check-input" type="checkbox" :checked="thing.completed" @input="toggleTodo(index)">
+              <label class="form-check-label" :class="{thing: thing.completed} ">{{thing.subject}}</label>
+              <!-- :style="thing.completed?todoStyle:{}" 으로 스타일을 바인딩해서 사용할 수도 있다.  -->
+              <!-- :class 속성을 바인딩할때 {클래스이름 : 속성} 에서 속성이 true일때만 클래스가 동작하게 된다.  -->
+            </div>
+            <div>
+              <button class="btn btn-danger btn-sm" @click="deletething(index)">Delete</button>
+            </div>
+          </div>
+      </div>
+    </div>
+</template>
+
+<script>
+//import { watchEffect } from 'vue';
+export default {
+    props:['todo'],
+    emits:['toggle-todo','delete-thing'],//emit으로 보낸값도 따로 적어주는것이 좋다. 
+// props로 todo를 부모컴포넌트로 부터 받아온다.
+    setup(props, {emit}){//context.emit을 {emit}으로 바로 사용가능
+      // watchEffect(() => {
+      //   console.log(props.todo.length);
+      // });
+        const toggleTodo=(index)=>{
+            emit('toggle-todo',index);
+            //부모컴포넌트로 자식컴포넌트의 값이 변경되었음을 알린다. 
+        };
+        const deletething=(index)=>{
+            emit('delete-thing',index);
+        }
+        return{
+            toggleTodo,deletething
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
