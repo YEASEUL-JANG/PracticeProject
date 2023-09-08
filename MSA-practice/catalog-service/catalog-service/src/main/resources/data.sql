f6750cc9100d9127fbb369e44b17b53355f1476a
ysjang <changys1212@naver.com>
2023-09-08 17:55:20 +0900

    create catalog-service project

---
+++ MSA-practice/catalog-service/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+values ('CATALOG-0001','BERLIN',100,1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+values ('CATALOG-0002','TOKYO',100,900);
+insert into catalog(product_id, product_name, stock, unit_price)
+values ('CATALOG-0003','STOCKHOLM',100,1200);
