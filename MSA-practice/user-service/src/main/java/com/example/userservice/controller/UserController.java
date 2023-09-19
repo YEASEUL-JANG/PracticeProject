10cea8bb8da3ab87d08b371411222c4cbe3ba17e
ysjang <changys1212@naver.com>
2023-09-19 14:41:07 +0900

    add kafka server

--- MSA-practice/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ MSA-practice/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -69,6 +69,7 @@ public class UserController {
     public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId){
         UserDto userDto = userService.getUserByUserId(userId);
         ResponseUser responseUser = new ModelMapper().map(userDto,ResponseUser.class);
+
         return ResponseEntity.ok(responseUser);
     }
 }
