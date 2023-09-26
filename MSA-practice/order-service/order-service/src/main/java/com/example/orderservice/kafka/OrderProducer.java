a0a981da94d973de991b2ef096bf64a5d39a8057
ysjang <changys1212@naver.com>
2023-09-26 15:17:34 +0900

    sync order kafka topic

--- MSA-practice/order-service/order-service/src/main/java/com/example/orderservice/kafka/OrderProducer.java
+++ MSA-practice/order-service/order-service/src/main/java/com/example/orderservice/kafka/OrderProducer.java
@@ -1,32 +1,55 @@
 package com.example.orderservice.kafka;
 
-import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.dto.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
+import java.util.*;
 @Service
 @Slf4j
 public class OrderProducer {
 
     private KafkaTemplate<String, String> kafkaTemplate;
-
+    List<Field> fields = Arrays.asList(new Field("string", true, "order_id"),
+            new Field("string", true, "user_id"),
+            new Field("string", true, "product_id"),
+            new Field("int32", true, "qty"),
+            new Field("int32", true, "total_price"),
+            new Field("int32", true, "unit_price"));
+    Schema schema = Schema.builder()
+            .type("struct")
+            .fields(fields)
+            .optional(false)
+            .name("orders")
+            .build();
     public OrderProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public OrderDto send(String kafkaTopic, OrderDto orderDto){
+
+        Payload payload = Payload.builder()
+                .order_id(orderDto.getOrderId())
+                .user_id(orderDto.getUserId())
+                .product_id(orderDto.getProductId())
+                .qty(orderDto.getQty())
+                .unit_price(orderDto.getUnitPrice())
+                .total_price(orderDto.getTotalPrice())
+                .build();
+        KafkaOrderDto kafkaOrderDto = new KafkaOrderDto(schema,payload);
+
         ObjectMapper mapper = new ObjectMapper();
         String jsonInString = "";
         try {
-            jsonInString = mapper.writeValueAsString(orderDto);
+            jsonInString = mapper.writeValueAsString(kafkaOrderDto);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
         kafkaTemplate.send(kafkaTopic, jsonInString);
-        log.info("Kafka Producer send data from the Order microservice: "+ orderDto);
+        log.info("Order Producer send data from the Order microservice: "+ kafkaOrderDto);
+
 
         return orderDto;
     }
