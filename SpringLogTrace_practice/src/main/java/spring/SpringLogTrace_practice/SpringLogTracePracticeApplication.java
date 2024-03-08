31b98d89f082a1b17316948561e2a1f99841e76d
YEASEUL JANG <changys1212@naver.com>
2024-03-08 22:33:50 +0900

    add spring-log-trace project

---
+++ SpringLogTrace_practice/src/main/java/spring/SpringLogTrace_practice/SpringLogTracePracticeApplication.java
@@ -0,0 +1,13 @@
+package spring.SpringLogTrace_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogTracePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogTracePracticeApplication.class, args);
+	}
+
+}
---
+++ SpringLogTrace_practice/src/test/java/spring/SpringLogTrace_practice/SpringLogTracePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.SpringLogTrace_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogTracePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
