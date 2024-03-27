8684784a5c2bb1857e424db7dcb25f5bad477ebb
YEASEUL JANG <changys1212@naver.com>
2024-03-27 18:50:09 +0900

    add strategy callback pattern test

---
+++ SpringLogTrace_practice/src/test/java/spring/SpringLogTrace_practice/trace/strategy/ContextV2Test.java
@@ -0,0 +1,53 @@
+package spring.SpringLogTrace_practice.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import spring.SpringLogTrace_practice.trace.strategy.code.*;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략패턴 이용 (execute() 시 파라미터값으로 전달)
+     */
+    @Test
+    void strategyV1(){
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
+    }
+    /**
+     *  전략패턴 - 익명 내부클래스 이용
+     */
+    @Test
+    void strategyV2(){
+        ContextV2 context1 = new ContextV2();
+        context1.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+
+        ContextV2 context2 = new ContextV2();
+        context2.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+    }
+
+
+    /**
+     *  전략패턴 - 더 간단히 사용하기 (람다사용)
+     *  -> 단 람다사용은 인터페이스에 구현할 메서드가 하나만 있어야 함.
+     */
+    @Test
+    void strategyV3(){
+
+        ContextV2 context1 = new ContextV2();
+        context1.execute(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
--- SpringLogTrace_practice/src/test/java/spring/SpringLogTrace_practice/trace/strategy/code/ContextV2.java
+++ SpringLogTrace_practice/src/test/java/spring/SpringLogTrace_practice/trace/strategy/code/ContextV2.java
@@ -7,17 +7,11 @@ import lombok.extern.slf4j.Slf4j;
  * 이 Context는 내부에 Strategy 필드를 가지고 있고 해당 구현체를 주입하면 된다.
  * 구현부분이 인터페이스에만 의존되어 있음.
  *
- * v1 : 전략을 주입받는 방식
+ * v2 : 전략을 파라미터로 전달받는 방식
  */
 @Slf4j
-public class ContextV1 {
-    private  Strategy strategy;
-
-    // 생성자를 통해 전략을 주입받음.
-    public ContextV1(Strategy strategy){
-        this.strategy = strategy;
-    }
-    public void execute(){
+public class ContextV2 {
+    public void execute(Strategy strategy){
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         strategy.call(); //위임
