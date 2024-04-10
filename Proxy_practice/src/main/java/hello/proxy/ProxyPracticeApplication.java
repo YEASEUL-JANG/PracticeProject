cd6cf18d24f52c91735b4ad50d90bcc051f6bae7
YEASEUL JANG <changys1212@naver.com>
2024-04-10 19:26:08 +0900

    add proxy v1

--- Proxy_practice/src/main/java/hello/proxy/ProxyPracticeApplication.java
+++ Proxy_practice/src/main/java/hello/proxy/ProxyPracticeApplication.java
@@ -1,8 +1,11 @@
 package hello.proxy;
 
+import hello.proxy.config.AppConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppConfigV1.class) // 클래스를 스프링 빈으로 등록한다.
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyPracticeApplication {
 
