9d01a42432d79e035375b29142dd9ffb31c10b72
YEASEUL JANG <changys1212@naver.com>
2024-05-17 12:42:18 +0900

    add pointcut test

--- ProxyPattern_practice/src/test/java/hello/proxy/advisor/AdviserTest.java
+++ ProxyPattern_practice/src/test/java/hello/proxy/advisor/AdviserTest.java
@@ -1,15 +1,47 @@
 package hello.proxy.advisor;
 
+import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 public class AdviserTest {
 
     @Test
     void advisorTest1(){
         ServiceInterface target = new ServiceImpl();
-        ProxyFactory proxyFactory = new ProxyFactory();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        //advisor 만들기
+        //Pointcut.TRUE : 항상 true를 반환하는 포인트컷
+        //포인트컷은 개발자가 구현할 일은 거의 없고 주로 스프링에서 제공하는 포인트컷을 이용함.
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
     }
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷")
+    void advisorTest2(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        //save 를 호출할 때는 advise가 적용되지만 find를 호출할 때는 적용되지 않음.
+        pointcut.setMappedNames("save");
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+
 }
